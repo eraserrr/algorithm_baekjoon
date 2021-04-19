00aee54d770082c44ee8bd804e08ccccda1d4811
jiwooPark <a01074761054@gmail.com>
2021-04-19 22:48:18 +0900

    Update 0419_1,2,3 더하기5(실패).py

--- dp/0419_1,2,3 더하기5(실패).py
+++ dp/0419_1,2,3 더하기5(실패).py
@@ -14,3 +14,27 @@ for i in range(4,test_max+1):
 
 for i in range(n):
     print(sum(dp[t[i]])%1000000009)
+
+    
+    
+## 성공 코드
+
+## 나누기  연산 주의!!!!!!!
+
+n = int(input())
+t = []
+for i in range(n):
+    t.append(int(input()))
+
+test_max = max(t)
+dp = [[0,0,0,0] for _ in range(test_max+1)]
+dp[1] = [0,1,0,0]
+dp[2] = [0,0,1,0]
+dp[3] = [0,1,1,1]
+for i in range(4,test_max+1):
+    dp[i][1] = (dp[i-1][2] + dp[i-1][3])%1000000009
+    dp[i][2] = (dp[i-2][1] + dp[i-2][3])%1000000009
+    dp[i][3] = (dp[i-3][1] + dp[i-3][2])%1000000009
+
+for i in range(n):
+    print(sum(dp[t[i]])%1000000009)
