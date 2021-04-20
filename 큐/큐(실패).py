d0c8b3e0c2433659f95ce3efc11dfad9e117919b
jiwooPark <a01074761054@gmail.com>
2021-04-20 14:45:15 +0900

    Update 큐(실패).py

--- 큐/큐(실패).py
+++ 큐/큐(실패).py
@@ -1,3 +1,6 @@
+# 입력받을 때에 sys.stdin.realine() 이용해야 통과할 수 있다
+# sys.stdin.readline() 은 맨 끝 개행 문자가 포함되는 것에 유의,
+
 n = int(input())
 s = ''
 queue = []
@@ -6,6 +9,52 @@ for _ in range(n):
     command = input()
     if command.find('push')==0:
         queue.append(command.split(' ')[1])
+        length += 1
+        continue
+    if command.find('pop')==0:
+        if length==0:
+            s += '-1\n'
+            continue
+        s += str(queue[0]) + '\n'
+        queue = queue[1:]
+        length -= 1
+        continue
+    if command.find('size')==0:
+        s += str(length) + '\n'
+        continue
+    if command.find('empty')==0:
+        if length==0:
+            s += '1\n'
+            continue
+        s += '0\n'
+        continue
+    if command.find('front')==0:
+        if length==0:
+            s += '-1\n'
+            continue
+        s += queue[0] + '\n'
+        continue
+    if command.find('back')==0:
+        if length==0:
+            s += '-1\n'
+            continue
+        s += queue[-1] + '\n'
+
+print(s)
+
+
+## 성공 코드
+
+import sys
+
+n = int(sys.stdin.readline())
+s = ''
+queue = []
+length = 0
+for _ in range(n):
+    command = sys.stdin.readline()
+    if command.find('push')==0:
+        queue.append(command.split(' ')[1][:-1])
         length += 1
         continue
     if command.find('pop')==0:
